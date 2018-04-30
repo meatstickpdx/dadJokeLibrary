ff67b477a8541f58eee03b92ed1673facd9785c7
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-30 15:32:20 -0700

    added user id to Question object

--- test/unit/Question.test.js
+++ test/unit/Question.test.js
@@ -1,6 +1,7 @@
 const { assert } = require('chai');
 const Question = require('../../lib/models/Question');
 const Answer = require('../../lib/models/Answer');
+const User = require('../../lib/models/User');
 const { getErrors } = require('./helpers');
 
 describe.only('Question model', () => {
@@ -9,16 +10,21 @@ describe.only('Question model', () => {
         content: 'It got mugged'
     };
 
+    const joe = {
+        username: 'Joe'
+    };
+
     const data = {
         prompt: 'Why did the coffee file a police report',
-        user: 75,
         status: 'submit'
     };
 
     const answer = new Answer(punchline);
+    const user = new User(joe);
 
     it('is a valid model', () => {
         data.answers = [answer._id];
+        data.user = user._id;
         const question = new Question(data);
         data._id = question._id;
         assert.deepEqual(question.toJSON(), data);
@@ -28,12 +34,13 @@ describe.only('Question model', () => {
 
     it('requires fields', () => {
         const question = new Question({});
-        const errors = getErrors(question.validateSync(), 1);
+        const errors = getErrors(question.validateSync(), 2);
         assert.equal(errors.prompt.kind, 'required');
+        assert.equal(errors.user.kind, 'required');
     });
 
     it('checks that status is an enum', () => {
-        const question = new Question({ prompt: 'This is a dad question', status: 'fail' });
+        const question = new Question({ prompt: 'This is a dad question', user: user._id, status: 'fail' });
         const errors = getErrors(question.validateSync(), 1);
         assert.equal(errors.status.kind, 'enum');
     });
