20c5edfd5ee6e737a72ee98883a5558c981dec59
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-30 15:26:17 -0700

    updated Question model with answer ids

--- test/unit/Question.test.js
+++ test/unit/Question.test.js
@@ -1,27 +1,37 @@
 const { assert } = require('chai');
 const Question = require('../../lib/models/Question');
+const Answer = require('../../lib/models/Answer');
 const { getErrors } = require('./helpers');
 
-describe('Question model', () => {
+describe.only('Question model', () => {
+
+    const punchline = {
+        content: 'It got mugged'
+    };
+
+    const data = {
+        prompt: 'Why did the coffee file a police report',
+        user: 75,
+        status: 'submit'
+    };
+
+    const answer = new Answer(punchline);
 
     it('is a valid model', () => {
-        const data = {
-            prompt: 'This is a dad question',
-            answers: ['dads are the best jokesters', 'dads tell horrible jokes'],
-            user: 75,
-            status: 'submit'
-        };
+        data.answers = [answer._id];
         const question = new Question(data);
         data._id = question._id;
         assert.deepEqual(question.toJSON(), data);
         assert.isUndefined(question.validateSync());
 
     });
+
     it('requires fields', () => {
         const question = new Question({});
         const errors = getErrors(question.validateSync(), 1);
         assert.equal(errors.prompt.kind, 'required');
     });
+
     it('checks that status is an enum', () => {
         const question = new Question({ prompt: 'This is a dad question', status: 'fail' });
         const errors = getErrors(question.validateSync(), 1);
