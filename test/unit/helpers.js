8763e8ba031ec0f7af704fe75bba36c29cb86e87
Katlyn Tucker <studioquirk@yahoo.com>
2018-04-30 11:29:23 -0700

    added helper file for  get errors function

---
+++ test/unit/helpers.js
@@ -0,0 +1,13 @@
+// const { assert } = require('chai');
+
+// const getErrors = (validation, expected) => {
+//     assert.isDefined(validation, 'expected validation errors');
+//     const { errors } = validation;
+//     assert.isDefined(errors);
+//     if(expected !== undefined) {
+//         assert.equal(Object.keys(errors).length, expected);
+//     }
+//     return errors;
+// };
+
+// module.exports = { getErrors };
