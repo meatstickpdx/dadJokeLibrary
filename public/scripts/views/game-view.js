df5b2bf53243fad783f7989f8fc8b4f220260e82
Jen Lipton <jkl.lipton@gmail.com>
2018-05-01 16:03:24 -0700

    pushing

--- public/scripts/views/game-view.js
+++ public/scripts/views/game-view.js
@@ -6,6 +6,8 @@
     const errorView = module.errorView;
     const handleError = err => errorView.init(err);
 
+    let currentQuestion = {};
+
     gameView.init = () => {
         $('#game-view').show();
 
@@ -13,12 +15,18 @@
 
         $.get( 'http://localhost:3000/questions', ( questions ) => {
             questions.length ? gameView.loadQuestion(questions) : gameView.questionError();
+        }).then( () => gameView.loadAnswers() );
+    },
+    
+    gameView.loadAnswers = () => {
+        $.get( `http://localhost:3000/questions/${currentQuestion._id}/answers`, ( answers ) => {
+            console.log('ANSWERS', answers);
         });
-
     },
 
     gameView.loadQuestion = (questions) => {
-        $('#question').append(`<h2>${questions[questions.length - 1].prompt}</h2>`);
+        currentQuestion = questions[questions.length - 1];
+        $('#question').append(`<h2>${currentQuestion.prompt}</h2>`);
     },
 
     gameView.questionError = () => {
