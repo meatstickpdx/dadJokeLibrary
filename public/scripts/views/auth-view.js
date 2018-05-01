cb2b042da8782b3c54225b1767718bbb2283a270
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-05-01 13:43:00 -0700

    wrote submit handler for signup

--- public/scripts/views/auth-view.js
+++ public/scripts/views/auth-view.js
@@ -1,11 +1,31 @@
 'use strict';
 
 (function (module) {
+    const User = require('../../../lib/models/User');
 
     const authView = {};
 
     authView.init = () => {
         $('#auth-view').show();
+        $('#sign-up').off('submit').on('submit', handleSignup);
+    };
+
+    const handleSignup = event => {
+        event.preventDefault();
+        const credentials = {
+            username: $('#email').val(),
+            password: $('#password').val()
+        };
+
+        User.signup(credentials)
+            .then(() => {
+                $('#email')[0].reset();
+                $('#password')[0].reset();
+                page('/');
+            })
+            .catch(err => {
+                console.log(err);
+            });
     };
 
     module.authView = authView;
