8a8c81238dacfa05608ac3da37c2b4381340a08b
Katlyn Tucker <studioquirk@yahoo.com>
2018-05-01 17:18:55 -0700

    adding front end routes to buttons and such

--- public/scripts/views/game-view.js
+++ public/scripts/views/game-view.js
@@ -6,7 +6,7 @@
     const errorView = module.errorView;
     const handleError = err => errorView.init(err);
 
-    let currentQuestion = {};
+    gameView.currentQuestion = {};
 
     gameView.init = () => {
         $('#game-view').show();
@@ -16,23 +16,51 @@
         $.get( 'http://localhost:3000/questions', ( questions ) => {
             questions.length ? gameView.loadQuestion(questions) : gameView.questionError();
         }).then( () => gameView.loadAnswers() );
-    },
+        $('#submitAnswer').off('click').on('click', handleAnswer);
+    };
     
     gameView.loadAnswers = () => {
-        $.get( `http://localhost:3000/questions/${currentQuestion._id}/answers`, ( answers ) => {
+        $.get( `http://localhost:3000/questions/${gameView.currentQuestion._id}/answers`, ( answers ) => {
             console.log('ANSWERS', answers);
         });
-    },
+    };
 
     gameView.loadQuestion = (questions) => {
-        currentQuestion = questions[questions.length - 1];
-        $('#question').append(`<h2>${currentQuestion.prompt}</h2>`);
-    },
+        gameView.currentQuestion = questions[questions.length - 1];
+        $('#question').append(`<h2>${gameView.currentQuestion.prompt}</h2>`);
+    };
 
     gameView.questionError = () => {
         $('#question').append('<h2>Please set a question on the Admin Page</h2>');
-    },
+    };
 
+    const handleAnswer = event => {
+        event.preventDefault();
+        const answer = {
+            content: $('#answer').val(),
+            question: gameView.currentQuestion._id
+        };
+        const token = window.localStorage.getItem('token');
+
+        fetch(`/answers`, {
+            body: JSON.stringify(answer),
+            headers: {
+                'token' : token,
+                'content-type': 'application/json'
+            },
+            method: 'POST',
+            mode: 'cors'
+        })
+            .then(response => response.json())
+            .then(res => {
+                console.log('res???', res);
+                $('#answers-form').trigger('reset');
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    };
+   
     module.gameView = gameView;
 
 })(window.module);
