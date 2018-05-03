ad9e95c30a38b7c50fccded91576bca0af682c15
Katlyn Tucker <studioquirk@yahoo.com>
2018-05-03 14:10:42 -0700

    changing aggregate a smidge for html

--- public/scripts/views/results-view.js
+++ public/scripts/views/results-view.js
@@ -18,7 +18,6 @@
         })
             .then(res => res.json())
             .then(res => {
-                console.log('res id', res._id);
                 fetch(`/votes/results?question=${res._id}`, {
                     headers: {
                         'token' : token,
@@ -29,7 +28,7 @@
                 })
                     .then(response => response.json())
                     .then(res => {
-                        console.log('RES', res);
+                        loadResults(res);
                         $('#results-view').show();
                     })
                     .catch(err => {
@@ -37,7 +36,12 @@
                     });
             });
     };
-
+    const loadResults = (resultsArray) => {
+        resultsArray.forEach(results => {
+            const resultsCard = resultsTemplate(results);
+            $(`#results-view`).append(resultsCard);
+        });
+    };
     module.resultsView = resultsView;
 
 })(window.module);
