456079a76dafb1e7edb854a9ffd56d866fb9af56
victorbofill <victorm.bofill@gmail.com>
2018-05-04 11:06:30 -0700

    env functionality added

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const http = require('http');
 const app = require('./lib/app');
 const connect = require('./lib/util/connect');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27019/dadjoke_test';
+const MONGODB_URI = process.env.MONGODB_URI;
 
 connect(MONGODB_URI);
 
--- test/e2e/db.js
+++ test/e2e/db.js
@@ -2,7 +2,7 @@ require('dotenv').config({ path: './.env'});
 const connect = require('../../lib/util/connect');
 const mongoose = require('mongoose');
 
-before(() => connect(process.env.MONGODB_URI || 'mongodb://localhost:27019/dadjoke_test'));
+before(() => connect(process.env.MONGODB_URI));
 after(() => mongoose.connection.close());
 
 module.exports = {
